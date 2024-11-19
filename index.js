da8600fffa1b33e7b3189eea66134cfd10a95ac8
saldanha <emanuelsaldanha17@hotmail.com>
2024-11-19 12:14:05 -0300

    projeto

---
+++ index.js
@@ -0,0 +1,53 @@
+const nome = document.querySelector('#primeiro')
+const email = document.querySelector('#email')
+const nol = document.querySelector('.nom')
+const form = document.querySelector('.form-group')
+
+function checar() {
+
+    if (nome.value.length <= 8) {
+        form.classList.add('estilo')
+        alert('Nome incompleto')
+
+    }
+
+    
+    if (!nome.value) {
+        return alert('Erro Nome Está Vazio')
+
+    }
+    if (!email.value) {
+        return alert(`Erro Email Está Vazio`)
+
+    }
+
+
+
+    else {
+        abrir()
+        form.classList.remove('estilo')
+    }
+}
+
+document.addEventListener('click', function (e) {
+    const el = e.target
+    if (el.classList.contains('checar')) {
+        checar()
+
+
+    }
+
+
+
+})
+
+
+
+
+
+function abrir() {
+    window.location.href = 'inde.html';
+
+}
+
+
